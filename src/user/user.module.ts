c22b33bf5bc8bf059463017387de75a5140b6d83
GumSurf <gabriel.christe@gmail.com>
2024-02-22 01:25:47 +0100

    Add: UserController dans controllers et configuration middleware pour AuthMiddleware

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { UserController } from './user.controller'; // Importez le UserController
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserService } from './user.service';
 import { UserSchema } from './user.entity'; // Importez le schema User
+import { AuthMiddleware } from '../auth/auth.middleware'; // Importez votre middleware d'authentification
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]), // Importez le schema User dans MongooseModule
-  ],
-  providers: [UserService],
-  exports: [UserService], // Exportez le UserService pour qu'il puisse être utilisé dans d'autres modules si nécessaire
+    imports: [
+        MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]), // Importez le schema User dans MongooseModule
+    ],
+    controllers: [UserController],
+    providers: [UserService],
+    exports: [UserService], // Exportez le UserService pour qu'il puisse être utilisé dans d'autres modules si nécessaire
 })
-export class UserModule {}
+
+export class UserModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(AuthMiddleware)
+            .forRoutes({ path: 'user/profile', method: RequestMethod.GET }); // Appliquer le middleware à la route GET 'user/profile'
+    }
+}
