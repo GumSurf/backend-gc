5822d6facf29bec409149d23e233b990aacc6024
GumSurf <gabriel.christe@gmail.com>
2024-07-29 15:48:54 +0200

    Add: le User Schéma

---
+++ src/user/user.schema.ts
@@ -0,0 +1,9 @@
+import * as mongoose from 'mongoose';
+
+export const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  role: { type: String, default: 'user' },
+  emailConfirmed: { type: Boolean, default: false }
+});
