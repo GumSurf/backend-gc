3d41a0e76911783d459b7259bf3847db0fc84130
GumSurf <gabriel.christe@gmail.com>
2024-07-29 15:45:47 +0200

    Add: systéme de vérification d'utilisateur et création du token JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,44 +2,41 @@ import * as jwt from 'jsonwebtoken';
 import { Injectable } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 
-const jwtSecret = process.env.JWT_SECRET;
-
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+    constructor(private readonly userService: UserService) { }
 
-  async login(username: string, password: string): Promise<string | null> {
-    const user = await this.userService.findByUsername(username);
-    if (user && user.password === password) {
-      const token = jwt.sign({ username: user.username, userId: user.id }, jwtSecret);
-      console.log("Return token");
-      return token;
-    } else {
-        console.log("Error Connexion");
-      return null;
+    async login(email: string, password: string): Promise<string | null> {
+        const user = await this.userService.findByEmail(email);
+        if (user && user.password === password) {
+            const token = jwt.sign({ email: user.email, userId: user.id }, process.env.JWT_SECRET);
+            console.log("Return token");
+            return token;
+        } else {
+            console.log("Error Connexion");
+            return null;
+        }
     }
-  }
 
-  async validateUser(username: string, password: string): Promise<any> {
-    // Vérifiez si l'utilisateur existe dans la base de données
-    const user = await this.userService.findByUsername(username);
-    if (user && user.password === password) {
-      // Retournez l'utilisateur si le mot de passe correspond
-      console.log("l'utilisateur existe");
-      return user;
+    async validateEmail(email: string, password: string): Promise<any> {
+        // Vérifiez si l'utilisateur existe dans la base de données
+        const user = await this.userService.findByEmail(email);
+        if (user && user.password === password) {
+            // Retournez l'utilisateur si le mot de passe correspond
+            console.log("l'utilisateur existe");
+            return user;
+        }
+        // Retournez null si l'utilisateur n'est pas trouvé ou si le mot de passe est incorrect
+        console.log("l'utilisateur n'existe pas");
+        return null;
     }
-    // Retournez null si l'utilisateur n'est pas trouvé ou si le mot de passe est incorrect
-    console.log("l'utilisateur n'existe pas");
-    return null;
-  }
 
-  async generateJwtToken(username: string, password: string): Promise<string | null> {
-    const user = await this.userService.findByUsername(username);
-    if (user && user.password === password) {
-        console.log("jwtSecret = ", jwtSecret);
-      const token = jwt.sign({ username: user.username, userId: user.id }, jwtSecret);
-      return token;
+    async generateJwtToken(email: string, password: string): Promise<string | null> {
+        const user = await this.userService.findByEmail(email);
+        if (user && user.password === password && user.emailConfirmed === true) {
+            const token = jwt.sign({ email: user.email, userId: user.id }, process.env.JWT_SECRET);
+            return token;
+        }
+        return null;
     }
-    return null;
-  }
 }
