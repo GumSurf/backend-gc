7479d756210ce5e2e94679471e3d8f164fa4ab19
GumSurf <gabriel.christe@gmail.com>
2024-07-29 15:41:38 +0200

    Delete: ancienne fonctionnalité d'api de tutoriels

--- src/tutorial/tutorial.schema.ts
+++
@@ -1,9 +0,0 @@
-import * as mongoose from 'mongoose';
-
-export const TutorialSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    text: { type: String, required: true },
-    keywords: [String],
-}, {
-    timestamps: true,
-});
