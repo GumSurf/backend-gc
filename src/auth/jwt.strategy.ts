8cc9faddfbd0ed9a5eca907eed7d1cbd4a785e3b
GumSurf <gabriel.christe@gmail.com>
2024-07-29 16:39:46 +0200

    Add: Remplacer par la variable environnement du secret token

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTcwNzkzMjU3MSwiaWF0IjoxNzA3OTMyNTcxfQ.2gHByHKJOo07qS-NlZEEsjiIsBafkAbdfSr1Y0iRw3Q', // Remplacez par votre clé secrète
+      secretOrKey: process.env.JWT_SECRET, // Remplacez par votre clé secrète
     });
   }
 
