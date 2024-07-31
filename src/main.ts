cc0e9dd2fd2f4414ebf3313269357b317e1ea30d
GumSurf <gabriel.christe@gmail.com>
2024-07-31 13:46:28 +0200

    Add: good url github.io

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
     const port = process.env.PORT || 4000;
 
     app.enableCors({
-        origin: ['https://gumsurf.github.io', 'http://localhost:3000'],
+        origin: ['https://gumsurf.github.io', 'http://localhost:3000', 'https://galaxy-code-backend.vercel.app'],
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
