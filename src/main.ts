53193563af4aa4f7d0c13817f3cdd82185ec56a5
GumSurf <gabriel.christe@gmail.com>
2024-08-01 16:47:57 +0200

    Add: Cors options for vercel frontend

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
     const port = process.env.PORT || 4000;
 
     app.enableCors({
-        origin: ['https://gumsurf.github.io', 'http://localhost:3000', 'https://galaxy-code-backend.vercel.app'],
+        origin: ['https://gumsurf.github.io', 'http://localhost:3000', 'https://galaxy-code-backend.vercel.app', 'https://galaxy-code-frontend.vercel.app/'],
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
