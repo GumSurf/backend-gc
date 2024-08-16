f4e6dd982a680a9efd5709d39526f6d1cc0a3543
GumSurf <gabriel.christe@gmail.com>
2024-08-16 13:02:45 +0200

    Add: test vérification token

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -1,28 +1,28 @@
-import { Controller, Post, Req, Res, HttpStatus } from '@nestjs/common';
-import { Request, Response } from 'express';
-import { ApiService } from './api.service'; // Assurez-vous que le service ApiService est correctement importé
+import { Controller, Post, Req, HttpException, HttpStatus } from '@nestjs/common';
+import { Request } from 'express';
+import { ApiService } from './api.service';
 
 @Controller('api')
 export class ApiController {
   constructor(private readonly apiService: ApiService) {}
 
   @Post('validate-token')
-  async validateToken(@Req() req: Request, @Res() res: Response) {
+  async validateToken(@Req() req: Request) {
     const token = req.headers.authorization?.split(' ')[1];
     if (!token) {
-      return res.status(HttpStatus.UNAUTHORIZED).json({ valid: false });
+      throw new HttpException({ valid: false }, HttpStatus.UNAUTHORIZED);
     }
 
     try {
       const isValid = await this.apiService.validateToken(token);
       if (isValid) {
-        return res.status(HttpStatus.OK).json({ valid: true });
+        return { valid: true };
       } else {
-        return res.status(HttpStatus.UNAUTHORIZED).json({ valid: false });
+        throw new HttpException({ valid: false }, HttpStatus.UNAUTHORIZED);
       }
     } catch (error) {
       console.error('Erreur lors de la validation du token:', error);
-      return res.status(HttpStatus.UNAUTHORIZED).json({ valid: false });
+      throw new HttpException({ valid: false }, HttpStatus.UNAUTHORIZED);
     }
   }
 }
